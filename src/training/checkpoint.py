f4c3b1fa4b54b5643fa9d8a3c538196dd3bbc0f3
Whilifer <raf5fa@yandex.ru>
2026-08-16 06:07:23 +0300

    updated checkpoint, added load_checkpoint

--- src/training/checkpoint.py
+++ src/training/checkpoint.py
@@ -15,3 +15,18 @@ def save_checkpoint(
         model.state_dict(),
         path,
     )
+
+
+def load_checkpoint(
+    model: nn.Module,
+    path: str | Path,
+    device: torch.device,
+) -> None:
+    path = Path(path)
+
+    state_dict = torch.load(
+        path,
+        map_location=device,
+    )
+
+    model.load_state_dict(state_dict)
